eaa3ccf992fa3ae204e5a0b68c136e968b3c66f0
System Administrator <root@Tamzids-MacBook-Pro.local>
2024-08-22 22:53:19 -0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {    
     webpack5: true,
     webpack: (config) => {
-      config.resolve.fallback = { fs: false };
+      config.resolve.fallback = { fs: false, path: false };
   
       return config;
     },
